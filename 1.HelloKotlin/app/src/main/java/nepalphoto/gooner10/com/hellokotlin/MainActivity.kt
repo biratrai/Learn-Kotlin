c270ba387f47ae8fbb6464d6a0aacd7261efc631
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-09-05 21:13:41 -0400

    adding function, variables, and setting text to textView

--- 1.HelloKotlin/app/src/main/java/nepalphoto/gooner10/com/hellokotlin/MainActivity.kt
+++ 1.HelloKotlin/app/src/main/java/nepalphoto/gooner10/com/hellokotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package nepalphoto.gooner10.com.hellokotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloKotlin = "Hello Kotlin!"
+
+        displayHelloKotlin(helloKotlin)
     }
+
+    private fun displayHelloKotlin(helloKotlin: String) {
+        hello_kotlin.text = helloKotlin
+    }
+
 }
