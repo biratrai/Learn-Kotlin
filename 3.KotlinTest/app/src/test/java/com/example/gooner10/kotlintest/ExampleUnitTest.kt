d555a917ad0d93a968c53598c5014dbd57e20a83
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-12-19 15:19:52 -0500

    adding first kotlin test

--- 3.KotlinTest/app/src/test/java/com/example/gooner10/kotlintest/ExampleUnitTest.kt
+++ 3.KotlinTest/app/src/test/java/com/example/gooner10/kotlintest/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.gooner10.kotlintest
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private var calculator: Calculator? = null
+
+    @Before
+    fun setUp() {
+        calculator = Calculator()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addTwoNumbers() {
+        val resultAdd = calculator!!.add(1.0, 2.0)
+        assertThat(resultAdd, `is`(equalTo(3.0)))
     }
 }
